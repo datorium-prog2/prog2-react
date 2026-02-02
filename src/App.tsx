0aa7b6ce50105c36c68a7340651c606280947759
raganu-beka <rebekapilscikova@gmail.com>
2026-02-02 13:21:16 +0200

    configure tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function HelloDatorium() {
+function Hello() {
   return <p>Prog2 ir ļoti cool!</p>;
 }
 
 // Funkcijas ir komponents
 // Komponented atgriež JSX (JavaScript XML) - HTML līdzīgu sintaksi JavaScript iekšā
 function App() {
+  // Tailwind klases - uzliek konkrētus CSS komponentei.
+  let headerStyle =
+    "text-3xl sm:text-4xl md:text-5xl font-extrabold tracking-tight text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent py-6";
+
   return (
     <div>
-      <h1>Hello from React!</h1>
-      <HelloDatorium />
+      <h1 className={headerStyle}>Prog2Social</h1>
+      <Hello />
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // content pasaka, kādi faili ir jāpstrādā tailwindam
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
