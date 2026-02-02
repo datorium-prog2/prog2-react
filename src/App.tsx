ab31eba0940fe6e7c5006a203419eddf5bca3137
raganu-beka <rebekapilscikova@gmail.com>
2026-02-02 14:03:12 +0200

    add hello endpoint to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import { useEffect, useState } from "react";
+import { getHello } from "./api";
+
 function Hello() {
-  return <p>Prog2 ir ļoti cool!</p>;
+  // izmantojam State, glabāt aplikācijas stāvokli (informāciju)
+  // const [mainīgā nosaukums, funkcija lai mainītu vērtību] = useState(noklusējuma vērtība)
+  const [message, setMessage] = useState("");
+
+  // izpildās vienu reizi, kad komponente ielādējas
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getHello();
+      setMessage(response.data);
+    };
+
+    fetchData();
+  }, []); // tukšs saraksts => izpildīt tikai vienu reizi
+
+  const pStyle =
+    "text-base sm:text-lg md:text-xl text-gray-800 dark:text-gray-100 leading-relaxed max-w-prose mx-auto px-6 py-3 rounded-lg bg-white border border-gray-200 dark:bg-gray-800 dark:border-gray-700 shadow-sm";
+  return <p className={pStyle}>{message}</p>;
 }
 
 // Funkcijas ir komponents
