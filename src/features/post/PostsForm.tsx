8ee6be0f1de9058b189368794bfcb8fa4f0234c9
raganu-beka <rebekapilscikova@gmail.com>
2026-03-16 13:03:59 +0200

    feat: implement PostsForm component with title input, textarea, and submit button

--- src/features/post/PostsForm.tsx
+++ src/features/post/PostsForm.tsx
@@ -1,3 +1,19 @@
 export function PostsForm() {
-  return <input />;
+  return (
+    <div className="flex flex-col gap-2 mb-6">
+      <input
+        className="border-2 rounded-lg px-4 py-2"
+        type="text"
+        placeholder="Title"
+      />
+      <textarea
+        className="border-2 rounded-lg px-4 py-2"
+        placeholder="Share your thoughts..."
+        rows={3}
+      ></textarea>
+      <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
+        Post!
+      </button>
+    </div>
+  );
 }
