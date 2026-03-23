dbfdb911367fe3c776adb8fc50d5c7b6dce3a217
raganu-beka <rebekapilscikova@gmail.com>
2026-03-23 12:39:34 +0200

    feat: update Post and Comment interfaces to use Author type for consistency

--- src/features/post/types.ts
+++ src/features/post/types.ts
@@ -1,6 +1,15 @@
+// šajā failā mēs definējam datu tipus (interfeisi/klases)
+// ko mēs sūtīsim vai saņemsim no API
+// šos pašus tipus varēs izmantot datu attēlošani
+
 export interface CreatePostRequest {
   title: string;
   content: string;
+}
+
+export interface Author {
+  id: number;
+  username: string;
 }
 
 export interface Post {
@@ -8,9 +17,13 @@ export interface Post {
   title: string;
   content: string;
   createdAt: string;
-  authors: {
-    id: number;
-    username: string;
-  };
+  author: Author;
   likeCount: number;
+}
+
+export interface Comment {
+  id: number;
+  content: string;
+  createdAt: string;
+  author: Author;
 }
